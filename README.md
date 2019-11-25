e2f8f69b34c5f720a2e54af0bb340cca1c0aa3f7
Miguel Carboni <miguel.carboni@gmail.com>
2019-11-25 00:40:07 -0300

    implementacion

---
+++ README.md
@@ -0,0 +1,61 @@
+# Cliente WSAA AFIP para Python
+
+Cliente ligero para autenticarse a un web service AFIP usando pyOpenSSL. Se usa esta librería porque es màs fàcil de instalar con cualquier SO. Es ligero porque no genera un mensaje S/MIME para obtener el pkcs7 ni llamar a la consola, sino que lo genera usando la librería.
+
+Este es un proyecto "escuela" hecho para entender el mecanismo de autenticación y también para contar con una implementación que pueda funcionar rápidamente en cualquier SO. No está pensado para usar en producciòn.
+
+Para poder aprovecharlo es necesario contar con un certificado pkcs12 generado como indica el instructivo de [certificados](http://www.afip.gob.ar/ws/documentacion/certificados.asp)
+
+El módulo funciona con el ambiente de homologación. Hay una variable de entorno WSAA_URL que permitiría cambiar la URL a otro ambiente. Huelga repetir que es solo para pruebas, nadie algo tan precario para producción. ;)
+
+Se distribuye bajo licencia GPLv3.
+
+## Instalación
+
+Hay que importar las librerías que usa:
+
+```
+pip install -r requirements.txt
+```
+
+## Uso
+
+Supongamos que el certificado que generamos esta en el archivo ejemplo.p12 con clave 1234 y queremos ingresar a ws_sr_constancia_inscripcion:
+
+```bash
+python wsaa.py ejemplo.p12 1234  
+```
+Si todo fue bien imprimirá la respuesta con el token y el sign.
+
+Si falla algo lo imprime. Si queremos mas visibilidad de lo que está pasando le asignamos un valor a la variable de entorno WSAA_DEBUG e imprimirá el mensaje enviado y el recibido.
+
+## Uso como módulo
+
+```python
+from wsaa import crear_tra, firmar_tra_con
+from zeep import Client
+from OpenSSL import crypto
+
+p12 = crypto.load_pkcs12(open('certificado.pfx', 'rb').read(), b'1234')
+base_url = 'https://wsaahomo.afip.gov.ar/ws/services/LoginCms'
+a = crear_tra('ws_sr_padron_a5')
+s = firmar_tra_con(a, pkcs12 = p12)
+client = Client(f'{base_url}?wsdl')
+mensaje = client.service.loginCms(in0=s)
+```
+
+## Descripción breve
+
+El módulo wsaa se compone de una función que genera el TRA, y otra que lo firma.
+
+Usando zeep como cliente SOAP accedemos al servicio.
+
+La ventaja respecto a otras implementaciones que andan por ahí, esta no depende de archivos en el filesystem ni de tomar la parte pkcs7 de un mensaje S/MIME, sino que directamente firma y lo envia.
+
+La función firmar_tra_con puede recibir el pkcs12 o la clave privada y el certificado por separado.
+
+## Soporte y aportes
+
+Si necesitás ayuda o se te ocurre alguna mejora o querés colaborar generá un issue del repositorio o un pull requests.
+
+Happy hacking!
---
+++ wsaa.py
@@ -0,0 +1,88 @@
+"""
+Módulo wsaa liviano
+Copyright (C) 2019 miguelius
+
+Este módulo funciona como una herramienta de consola o librería para tu código.
+
+Se conecta al ambiente de homologación o puede cambiarse con la variable WSAA_URL.
+
+Todos los derechos reservados por miguelius y distribuido bajo licencia GPLv3.
+"""
+from zeep import Client
+from zeep.plugins import HistoryPlugin
+from sys import argv,exit
+from OpenSSL import crypto
+import base64
+from datetime import datetime
+from time import time
+import xml.etree.ElementTree as ET
+import os
+
+AMBIENTE = os.getenv("WSAA_URL", 'https://wsaahomo.afip.gov.ar/ws/services/LoginCms')
+DEBUG = os.getenv("WSAA_DEBUG", None)
+
+def loguear(mensaje):
+  if DEBUG is not None:
+    print(mensaje)
+
+def crear_tra(servicio, segundos_expiracion = 60):
+  now = int(time())
+  #now = int(datetime.timestamp(datetime.now()))
+  generacion = datetime.fromtimestamp(now - 60).isoformat()
+  expiracion = datetime.fromtimestamp(now + segundos_expiracion).isoformat()
+
+  return f"""<?xml version="1.0" encoding="UTF-8"?>
+    <loginTicketRequest version="1.0">
+      <header>
+        <uniqueId>{now}</uniqueId>
+        <generationTime>{generacion}-03:00</generationTime>
+        <expirationTime>{expiracion}-03:00</expirationTime>
+      </header>
+      <service>{servicio}</service>
+    </loginTicketRequest>
+  """
+
+def firmar_tra_con(tra, pkcs12=None, cert=None, key=None):
+  if pkcs12 is not None:
+    cert = pkcs12.get_certificate()
+    key = pkcs12.get_privatekey()
+  elif cert is None or key is None:
+    raise RuntimeException('No tengo con que firmar :(')
+
+  bio_in = crypto._new_mem_buf(tra.encode())
+  PKCS7_NOSIGS = 0x4
+  pkcs7 = crypto._lib.PKCS7_sign(cert._x509, key._pkey, crypto._ffi.NULL, bio_in, PKCS7_NOSIGS)
+  bio_out = crypto._new_mem_buf()
+  crypto._lib.i2d_PKCS7_bio(bio_out, pkcs7)
+  sigbytes = crypto._bio_to_string(bio_out)
+  signed_data = base64.b64encode(sigbytes)
+  return signed_data
+
+def main(certificado,passwd,servicio):
+  try:
+    p12 = crypto.load_pkcs12(open(certificado, 'rb').read(), passwd.encode('ascii'))
+    base_url = AMBIENTE
+    a = crear_tra(servicio)
+    s = firmar_tra_con(a, pkcs12 = p12)
+    history = HistoryPlugin()
+    client = Client(f'{base_url}?wsdl', plugins=[history])
+    mensaje = client.service.loginCms(in0=s)
+  except Exception as e:
+    mensaje = str(e)
+  finally:
+    loguear("envie: %s"%ET.tostring(history.last_sent['envelope']).decode('utf8'))
+    loguear("recibi: %s"%ET.tostring(history.last_received['envelope']).decode('utf8'))
+    print(mensaje)
+
+def imprimir_uso(eje):
+  print(f"""{eje} uso:
+{eje} ruta_al_certificado_pkcs12 pass id_servicio
+
+Imprime el ticket con las credenciales o arroja la excepcion encontrada.
+  """)
+
+if __name__ == '__main__':
+  if len(argv) != 4:
+    imprimir_uso(argv[0])
+    exit(0)
+  main(argv[1], argv[2], argv[3])
