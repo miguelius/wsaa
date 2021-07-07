547ba7b07626bac82c55973ea9b624fd39edf916
Miguel Carboni <miguel.carboni@gmail.com>
2021-07-07 14:15:03 -0300

    se cambio zeep por suds por compatibilidad. ahora ya no necesita el pkcs12, le alcanza con la clave publica y privada

--- wsaa.py
+++ wsaa.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#/usr/bin/env python
+
 """
 Módulo wsaa liviano
 Copyright (C) 2019 miguelius
@@ -10,15 +10,17 @@ Se conecta al ambiente de homologación o puede cambiarse con la variable WSAA_U
 
 Todos los derechos reservados por miguelius y distribuido bajo licencia GPLv3.
 """
-from zeep import Client
-from zeep.plugins import HistoryPlugin
 from sys import argv,exit
 from OpenSSL import crypto
 import base64
 from datetime import datetime
 from time import time
-import xml.etree.ElementTree as ET
 import os
+from suds.client import Client
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.getLogger('suds.client').setLevel(logging.DEBUG)
+
 
 AMBIENTE = os.getenv("WSAA_URL", 'https://wsaahomo.afip.gov.ar/ws/services/LoginCms')
 DEBUG = os.getenv("WSAA_DEBUG", None)
@@ -48,7 +50,7 @@ def firmar_tra_con(tra, pkcs12=None, cert=None, key=None):
     cert = pkcs12.get_certificate()
     key = pkcs12.get_privatekey()
   elif cert is None or key is None:
-    raise RuntimeException('No tengo con que firmar :(')
+    raise Exception('No tengo con que firmar :(')
 
   bio_in = crypto._new_mem_buf(tra.encode())
   PKCS7_NOSIGS = 0x4
@@ -59,31 +61,38 @@ def firmar_tra_con(tra, pkcs12=None, cert=None, key=None):
   signed_data = base64.b64encode(sigbytes)
   return signed_data
 
-def main(certificado,passwd,servicio):
+def main(servicio, ruta_certificado, ruta_clave, passwd):
   try:
-    p12 = crypto.load_pkcs12(open(certificado, 'rb').read(), passwd.encode('ascii'))
+    #p12 = crypto.load_pkcs12(open(certificado, 'rb').read(), passwd.encode('ascii'))
+
+    with open(ruta_certificado, 'r+') as f:
+      cert = crypto.load_certificate(crypto.FILETYPE_PEM, f.read())
+
+    with open(ruta_clave, 'r+') as f:
+      key = crypto.load_privatekey(crypto.FILETYPE_PEM, f.read(), passwd)
+
     base_url = AMBIENTE
     a = crear_tra(servicio)
-    s = firmar_tra_con(a, pkcs12 = p12)
-    history = HistoryPlugin()
-    client = Client('%s?wsdl'%base_url, plugins=[history])
-    mensaje = client.service.loginCms(in0=s)
+    s = firmar_tra_con(a, cert=cert, key=key)
+    client = Client('%s?wsdl'%base_url)
+    mensaje = client.service.loginCms(s.decode('ascii'))
   except Exception as e:
     mensaje = str(e)
   finally:
-    loguear("envie: %s"%ET.tostring(history.last_sent['envelope']).decode('utf8'))
-    loguear("recibi: %s"%ET.tostring(history.last_received['envelope']).decode('utf8'))
     print(mensaje)
 
 def imprimir_uso(eje):
   print("""%s uso:
-%s ruta_al_certificado_pkcs12 pass id_servicio
+%s id_servicio certificate private_key pass
 
 Imprime el ticket con las credenciales o arroja la excepcion encontrada.
   """%(eje,eje))
 
 if __name__ == '__main__':
-  if len(argv) != 4:
+  if len(argv) < 4:
     imprimir_uso(argv[0])
     exit(0)
-  main(argv[1], argv[2], argv[3])
+  passwd = None
+  if 4 in argv:
+    passwd = argv[4].decode('ascii')
+  main(argv[1], argv[2], argv[3], passwd)
